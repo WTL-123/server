59b6a8745bb0ec453f69c4e5e3093328668aa996
WTL-123 <329026734@qq.com>
2020-11-21 21:44:26 +0800

    第一次提交

---
+++ src/com/wtl/server/Server.java
@@ -0,0 +1,22 @@
+package com.wtl.server;
+
+/**
+ * 使用ServerSocket建立与浏览器的连接，获取请求协议
+ */
+public class Server {
+    public static void main(String[] args) {
+
+    }
+//    启动服务器
+    public void start(){
+
+    }
+//    接收连接处理
+    public void receive(){
+
+    }
+//    停止服务器
+    public void stop(){
+
+    }
+}
